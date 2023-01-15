2e03f43547a2a448bbaa6907c8fffa2e61e4dbdb
zach-conrad <zconrad119@gmail.com>
2023-01-15 16:55:20 -0600

    Reformatted code.

--- cogs/Modtools.py
+++ cogs/Modtools.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import json
 
+
 class Modtools(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -11,58 +12,51 @@ class Modtools(commands.Cog):
     async def on_ready(self):
         print("Modtools.py is ready.")
 
-    @commands.command(aliases= ["purge", "c"])
-    @commands.has_permissions(manage_messages= True)
+    @commands.command(aliases=["purge", "c"])
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, count: int):
-        await ctx.channel.purge(limit= count)
+        await ctx.channel.purge(limit=count)
         await ctx.author.send(f"Cleared {count} messages.")
 
-    #Command to kick members #TODO Make slash command.
+    # Command to kick members #TODO Make slash command.
     @commands.command()
-    @commands.has_permissions(kick_members= True)
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, modreason):
         await ctx.guild.kick(member)
 
+        conf_embed = discord.Embed(title="Success!", color=discord.Color.green())
+        conf_embed.add_field(name="Kicked", value=f"{member.mention}", inline=False)
+        conf_embed.add_field(name="Reason", value=modreason, inline=False)
+        conf_embed.add_field(name="Punisher", value=ctx.author.name, inline=False)
+        await ctx.send(embed=conf_embed)
 
-        conf_embed = discord.Embed(title= "Success!", color=discord.Color.green())
-        conf_embed.add_field(name= "Kicked", value= f"{member.mention}", inline= False)
-        conf_embed.add_field(name= "Reason", value= modreason, inline= False)
-        conf_embed.add_field(name="Punisher", value= ctx.author.name, inline= False)
-        await ctx.send(embed= conf_embed)
-
-    #Command to ban members #TODO Make slash command.
+    # Command to ban members #TODO Make slash command.
     @commands.command()
-    @commands.has_permissions(ban_members= True)
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, modreason):
         await ctx.guild.ban(member)
 
-        conf_embed = discord.Embed(title= "Success!", color=discord.Color.green())
-        conf_embed.add_field(name= "Banned", value= f"{member.mention}", inline= False)
-        conf_embed.add_field(name= "Reason", value= modreason, inline= False)
-        conf_embed.add_field(name="Punisher", value= ctx.author.name, inline= False)
-        await ctx.send(embed= conf_embed)
+        conf_embed = discord.Embed(title="Success!", color=discord.Color.green())
+        conf_embed.add_field(name="Banned", value=f"{member.mention}", inline=False)
+        conf_embed.add_field(name="Reason", value=modreason, inline=False)
+        conf_embed.add_field(name="Punisher", value=ctx.author.name, inline=False)
+        await ctx.send(embed=conf_embed)
 
-    #Command to unban members #TODO Make slash command.
-    @commands.command(name= "unban")
+    # Command to unban members #TODO Make slash command.
+    @commands.command(name="unban")
     @commands.guild_only()
-    @commands.has_permissions(ban_members= True)
+    @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, userId):
-        user = discord.Object(id= userId)
+        user = discord.Object(id=userId)
         await ctx.guild.unban(user)
 
-        conf_embed = discord.Embed(title= "Success!", color=discord.Color.green())
-        conf_embed.add_field(name= "Unbanned", value= f"{userId.mention}", inline= False)
-        conf_embed.add_field(name="Issuer", value= ctx.author.name, inline= False)
-        await ctx.send(embed= conf_embed)
+        conf_embed = discord.Embed(title="Success!", color=discord.Color.green())
+        conf_embed.add_field(name="Unbanned", value=f"{userId.mention}", inline=False)
+        conf_embed.add_field(name="Issuer", value=ctx.author.name, inline=False)
+        await ctx.send(embed=conf_embed)
 
-    #Mute Command TODO - Use it inside of a database
-
-
-
-
-
+    # Mute Command TODO - Use it inside of a database
 
 
 async def setup(client):
     await client.add_cog(Modtools(client))
-
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 import asyncio
 import json
 
-intents = discord.Intents(value = 8)
-client = commands.Bot(command_prefix = "!",intents = discord.Intents.all())
+intents = discord.Intents(value=8)
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
-#Syncs all bot commands, sends a message to console saying if bot is online.
+# Syncs all bot commands, sends a message to console saying if bot is online.
 @client.event
 async def on_ready():
     await client.tree.sync()
@@ -21,7 +21,7 @@ async def on_ready():
         print(e)
 
 
-#On guild join events.
+# On guild join events.
 @client.event
 async def on_guild_join(guild):
     with open("/Users/zach/Documents/College/Comp Sci 1/CarnivalBot/cogs/jsonfiles/mutes.json", "r") as f:
@@ -29,9 +29,10 @@ async def on_guild_join(guild):
 
         mute_role[str(guild.id)] = None
 
-    #Checks if there is any one in the mute roles.
+    # Checks if there is any one in the mute roles.
     with open("/Users/zach/Documents/College/Comp Sci 1/CarnivalBot/cogs/jsonfiles/mutes.json", "w") as f:
-        json.dump(mute_role, f, indent= 4)
+        json.dump(mute_role, f, indent=4)
+
 
 @client.event
 async def on_guild_remove(guild):
@@ -44,17 +45,16 @@ async def on_guild_remove(guild):
         json.dump(mute_role, f, indent=4)
 
 
-#Loads all of the cogs
+# Loads all of the cogs
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await client.load_extension(f"cogs.{filename[:-3]}")
 
 
-#Main function that runs.
+# Main function that runs.
 async def main():
     async with client:
-
         with open("/Users/zach/Documents/College/Comp Sci 1/CarnivalBot/cogs/jsonfiles/config.json", "r") as f:
             data = json.load(f)
             token = data["TOKEN"]
@@ -62,5 +62,5 @@ async def main():
         await load()
         await client.start(token)
 
-asyncio.run(main())
 
+asyncio.run(main())
