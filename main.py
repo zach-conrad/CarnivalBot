ed23f792446623ce1a0abfc31a2de1b62b32e105
zach-conrad <zconrad119@gmail.com>
2023-01-15 11:17:43 -0600

    Added various comments explaining things in main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ intents = discord.Intents(value = 8)
 client = commands.Bot(command_prefix = "!",intents = discord.Intents.all())
 
 
+#Syncs all bot commands, sends a message to console saying if bot is online.
 @client.event
 async def on_ready():
     await client.tree.sync()
@@ -19,6 +20,8 @@ async def on_ready():
     except Exception as e:
         print(e)
 
+
+#On guild join events.
 @client.event
 async def on_guild_join(guild):
     with open("/Users/zach/Documents/College/Comp Sci 1/CarnivalBot/cogs/jsonfiles/mutes.json", "r") as f:
@@ -26,6 +29,7 @@ async def on_guild_join(guild):
 
         mute_role[str(guild.id)] = None
 
+    #Checks if there is any one in the mute roles.
     with open("/Users/zach/Documents/College/Comp Sci 1/CarnivalBot/cogs/jsonfiles/mutes.json", "w") as f:
         json.dump(mute_role, f, indent= 4)
 
@@ -39,11 +43,15 @@ async def on_guild_remove(guild):
     with open("/Users/zach/Documents/College/Comp Sci 1/CarnivalBot/cogs/jsonfiles/mutes.json", "w") as f:
         json.dump(mute_role, f, indent=4)
 
+
+#Loads all of the cogs
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await client.load_extension(f"cogs.{filename[:-3]}")
 
+
+#Main function that runs.
 async def main():
     async with client:
 
